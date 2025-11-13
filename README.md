5be3f67dbea3d282cff04bfa938c24613acd049c
RobertWesner <robert@wesner.io>
2025-11-13 21:29:47 +0100

    Cleanup.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # YouTube-Play-All-Standalone
-Standalone extension for the YouTube Play All userscript
+
+Standalone extension for the YouTube Play All userscript.
+
+## Preparing
+
+- `npm i`
+
+## Building
+
+- `php prepare.php` -> downloads the script.user.js and fixes it to be loadable
+- `npm run zip` -> builds the extension
--- wxt.config.js
+++ wxt.config.js
@@ -11,7 +11,18 @@ export default defineConfig({
         permissions: ['storage'],
         host_permissions: ['https://*.youtube.com/*', 'https://ytplaylist.robert.wesner.io/*'],
         browser_specific_settings: {
-            gecko: { id: 'ytpa-standalone@scripts.yt', strict_min_version: '128.0' },
+            gecko: {
+                id: 'ytpa-standalone@scripts.yt',
+                strict_min_version: '128.0',
+                data_collection_permissions: {
+                    "required": [
+                        "websiteContent",
+                        "websiteActivity",
+                        "browsingActivity",
+                        "personallyIdentifyingInfo", // only for the playlist emulation... but required I guess
+                    ],
+                },
+            },
         },
         content_security_policy: {
             extension_pages: 'script-src \'self\'; object-src \'none\';',
